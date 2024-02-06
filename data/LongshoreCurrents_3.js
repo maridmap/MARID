9e172179ca3dbf8815bc01744fee0935919d7cad
maridmap <157534388+maridmap@users.noreply.github.com>
2024-02-06 15:18:45 +0100

    Add files via upload

---
+++ data/LongshoreCurrents_3.js
@@ -0,0 +1,10 @@
+var json_LongshoreCurrents_3 = {
+"type": "FeatureCollection",
+"name": "LongshoreCurrents_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Title": "Bedload transport in the Vistula River mouth derived from dune migration rates, southern Baltic Sea.", "Authors": "Lisimenka, A., & Kubicki, A.", "Year": "2019", "DOI": "https://doi.org/10.1016/j.oceano.2019.02.003", "Journal": "Oceanologia, 61(3), 384-394", "Processes": "wave currents", "BF Height": "up to 0.71 m", "BF Length": "up to 35.4 m", "BF Shape": "near-triangular" }, "geometry": { "type": "Point", "coordinates": [ 18.95020032, 54.35930899 ] } },
+{ "type": "Feature", "properties": { "Title": "Large sediment waves over the Gulf of Roses upper continental slope (NW Mediterranean).", "Authors": "Ribó, M., Durán, R., Puig, P., Van Rooij, D., Guillén, J., & Masqué, P.", "Year": "2018", "DOI": "https://doi.org/10.1016/j.margeo.2018.02.006", "Journal": "Marine Geology, 399, 84-96", "Processes": "bottom currents and along-shore currents", "BF Height": "60 m", "BF Length": "2000 m", "BF Shape": "asymmetric" }, "geometry": { "type": "Point", "coordinates": [ 3.435390282, 42.24017648 ] } },
+{ "type": "Feature", "properties": { "Title": "Sand ridge morphology and bedform migration patterns derived from bathymetry and backscatter on the inner-continental shelf offshore of Assateague Island, USA.", "Authors": "Pendleton, E. A., Brothers, L. L., Thieler, E. R., & Sweeney, E. M.", "Year": "2017", "DOI": "https://doi.org/10.1016/j.csr.2017.06.021", "Journal": "Continental Shelf Research, 144, 80-97", "Processes": "storms, along-shore currents, tidal currents", "BF Height": "10-14 m", "BF Length": "8000-16000 m", "BF Shape": "a slightly crescentic-shape, nearly symmetric" }, "geometry": { "type": "Point", "coordinates": [ -75.26848066, 37.94577465 ] } }
+]
+}
---
+++ data/TurbidityCurrents_2.js
@@ -0,0 +1,8 @@
+var json_TurbidityCurrents_2 = {
+"type": "FeatureCollection",
+"name": "TurbidityCurrents_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Title": "Sea floor bedforms and their influence on slope accommodation.", "Authors": "Maselli, V., Kneller, B., Taiwo, O. L., & Iacopini, D.", "Year": "2019", "DOI": "https://doi.org/10.1016/j.marpetgeo.2019.01.021", "Journal": "Marine and Petroleum Geology, 102, 625-637", "Processes": "turbidity flows", "BF Height": "up to 100 m", "BF Length": "up to 1000 m", "BF Shape": "convex up stoss sides" }, "geometry": { "type": "Point", "coordinates": [ -37.5284041, -3.682047087 ] } }
+]
+}
